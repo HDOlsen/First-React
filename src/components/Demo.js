5a4c2420191407fff9b845b0970ea529d0441e99
HDOlsen <38845610+HDOlsen@users.noreply.github.com>
2018-07-16 15:19:00 -0500

    Add files via upload

---
+++ src/components/Demo.js
@@ -0,0 +1,25 @@
+
+
+import React, {Component} from 'react'
+
+export class Demo extends Component {
+
+  render() {
+
+    return (
+
+        <div className="bottom_wrapper">
+            <h2>Hello Watchkit</h2>
+            <p>Last month Apple released the anticipated Watchkit framework for developers in the form of iOS 8.2 beta SDK release.  The WatchKit framework enable the developers to create Apple Watch applications.  In this article we are going to focus 
+            on the basics of getting started with the WatchKit framework and developing apps for the Apple Watch.
+            </p>
+            <h1>12 comments 124 likes</h1>
+            <h2>Introduction to Swift</h2>
+            <p>Swift is a modern programming language developed by Apple to create the next generation of iOS and OSX applications.  Swift is a fairly new language and still in development but it clearly reflects
+            the intentions and the future directions.  This article will revolve around the basic concepts in the Swift language and how you can get started.
+            </p>
+            <h1>15 comments 45 likes</h1>
+        </div>
+        )
+    }
+}
---
+++ src/components/Menu.js
@@ -0,0 +1,19 @@
+
+
+import React, {Component} from 'react'
+
+export class Menu extends Component {
+
+  render() {
+
+    return (
+
+        <div className="navbar_wrapper">
+            <h2>HighOnCoding</h2> 
+            <p><b>Home</b></p> 
+            <p><a href="hccategoriesreal.html">Categories</a></p> 
+        </div>
+
+    )
+  }
+}
